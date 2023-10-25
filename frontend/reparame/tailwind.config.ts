856a0b4df3c94224662270784d30f6a642ab7219
Tomas <tomaasvillagra@gmail.com>
2023-10-25 16:47:03 -0300

    feat: add Home screen and refactor code

--- frontend/reparame/tailwind.config.ts
+++ frontend/reparame/tailwind.config.ts
@@ -1,35 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        'mega-light-orange': '#fac79a',
-        'light-orange': '#ff983f',
-        'dark-orange': '#ff6600',
-        'red': '#fca5a5',
-        'red2': '#ef4444',
-        'orange': '#fdba74',
-        'grayUi': '#f5f5f5',
+        "mega-light-orange": "#fac79a",
+        "light-orange": "#ff983f",
+        "dark-orange": "#ff6600",
+        red: "#fca5a5",
+        red2: "#ef4444",
+        orange: "#fdba74",
+        grayUi: "#f5f5f5",
       },
       maxWidth: {
-        'max-view': '1520px',
-        'max-textArea': '778px',
+        "max-view": "1520px",
+        "max-textArea": "778px",
       },
       minWidth: {
-        'avatarHeader': '2.5rem',
-        'searchBar': '180px',
+        avatarHeader: "2.5rem",
+        searchBar: "180px",
       },
-    }
+      boxShadow:{
+        cardProfesionHome: "2px 3px 12px 0px #959393",
+        circle:"0px 4px 0px 0px #b0b0b0",
+        spanCircle:"0px 4px 5px 0px #b0b0b0"
+      }
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
-}
-export default config
+};
+export default config;
