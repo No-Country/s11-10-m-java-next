014e8fd2dd67c8057c3ee01ae4e2593c9307f129
nzaeta <nicocpla@hotmail.com>
2023-10-25 22:27:50 -0300

    prueba cors

--- backend/src/main/java/com/reparame/demo/Config/CorsConfig.java
+++ backend/src/main/java/com/reparame/demo/Config/CorsConfig.java
@@ -1,23 +1,32 @@
 package com.reparame.demo.Config;
 
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
+@Component
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer CORSConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT","DELETE");
-            }
-        };
+//    @Bean
+	
+public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*"); 
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(request, response);
     }
 
 }
