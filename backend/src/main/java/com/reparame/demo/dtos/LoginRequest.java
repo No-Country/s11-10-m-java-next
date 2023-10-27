058285fbd3420220d65b4f56e7122035f72884d4
nachova1 <nachou.pk@hotmail.com>
2023-10-27 11:29:21 -0300

    lo necesitaba para hacer pull. no modifique nada

--- backend/src/main/java/com/reparame/demo/dtos/LoginRequest.java
+++ backend/src/main/java/com/reparame/demo/dtos/LoginRequest.java
@@ -18,11 +18,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
-    @NotBlank(message = "El username  no debe estar en blanco o nulo")
+    @NotBlank(message = "El correo electronico no debe estar en blanco o nulo")
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "La dirección de correo electrónico no es válida")
     String username;
     
-    @NotBlank(message = "El correo electrónico no debe estar en blanco o nulo")
+    @NotBlank(message = "La contrase\u00F1a no debe estar en blanco o nulo")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,15}$", message = "La contrase\u00F1a no cumple con los requisitos, ingrese letras mayusculas y minusculas, numeros, un caracter especial y maximo 15")         
     String password;
 
