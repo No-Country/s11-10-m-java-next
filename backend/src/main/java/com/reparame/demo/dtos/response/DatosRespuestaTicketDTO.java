8a7504fdd0af7a56a6e9a1b8102f41df15496215
nzaeta <nicocpla@hotmail.com>
2023-10-31 11:51:36 -0300

    agregado campos a endpoint tickets

--- backend/src/main/java/com/reparame/demo/dtos/response/DatosRespuestaTicketDTO.java
+++ backend/src/main/java/com/reparame/demo/dtos/response/DatosRespuestaTicketDTO.java
@@ -8,13 +8,14 @@ import com.reparame.demo.entity.Cliente;
 import com.reparame.demo.entity.Servicio;
 import com.reparame.demo.entity.Ticket;
 import com.reparame.demo.enumeradores.ProgresoTicket;
+import com.reparame.demo.enumeradores.Rubros;
 
-public record DatosRespuestaTicketDTO( Long id , Boolean estado, String descripcion, LocalDate fechaInicio,  LocalDate fechaRequerida, ProgresoTicket progreso) {
+public record DatosRespuestaTicketDTO( Long id , Boolean estado, String descripcion, LocalDate fechaInicio,  LocalDate fechaRequerida, ProgresoTicket progreso, Rubros rubro, String zona, String nombrecompleto) {
 
 	
 	public DatosRespuestaTicketDTO(Ticket ticket) {
         this(ticket.getId_Tiket(), ticket.getEstado(), ticket.getDescripcion(), 
-        ticket.getFechaInicio(), ticket.getFechaRequerida(), ticket.getProgreso() );
+        ticket.getFechaInicio(), ticket.getFechaRequerida(), ticket.getProgreso(), ticket.getServicio().getRubro(), ticket.getServicio().getPrestador().getZona(), ticket.getServicio().getPrestador().getNombreCompleto() + " " + ticket.getServicio().getPrestador().getApellidoCompleto() );
     }
 	
 
