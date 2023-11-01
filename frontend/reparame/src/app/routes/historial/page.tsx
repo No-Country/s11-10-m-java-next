5b1065e5780b94b31a5e92f0fac1b18ade820ce0
PaulaVelezz <velezpaula.a@gmail.com>
2023-11-01 01:15:38 -0300

    reseñas prestador actualizadas, peticion get tickets actualizada con token

--- frontend/reparame/src/app/routes/historial/page.tsx
+++ frontend/reparame/src/app/routes/historial/page.tsx
@@ -1,12 +1,9 @@
 import Historial from "@/components/historial/Historial";
-import Ticket from "@/components/ticket/Ticket";
 import React from "react";
 
 const HistorialPage = () => {
   return (
     <section>
-      <Ticket />
-
       <Historial />
     </section>
   );
--- frontend/reparame/src/utils/requestTicket/getTickets.ts
+++ frontend/reparame/src/utils/requestTicket/getTickets.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 export const getTickets = async (setTickets: Function) => {
+  const tokenKey = localStorage.getItem("tKeyId");
   await axios
-    .get(`https://reparame-api.onrender.com/tickets`)
-
+    .get(`https://reparame-api.onrender.com/tickets`, {
+      headers: {
+        Authorization: `Bearer ${tokenKey}`,
+      },
+    })
     .then(function (response) {
       console.log(response);
       setTickets(response.data);
