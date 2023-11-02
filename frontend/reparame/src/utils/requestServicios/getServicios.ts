b12174bb648c89fc5dde5cd08e8df3080d4f3bbf
PaulaVelezz <velezpaula.a@gmail.com>
2023-11-01 23:28:54 -0300

    actualizo rama

--- frontend/reparame/src/utils/requestServicios/getServicios.ts
+++ frontend/reparame/src/utils/requestServicios/getServicios.ts
@@ -1,13 +1,21 @@
 import axios from "axios";
 
-export const getServicios = async (setPrestadores: Function, servicioId: any, token: string) => {
-    const config = {
-        headers: { Authorization: `Bearer ${token}` }
-    };
+export const getServicios = async (
+  setPrestadores: Function,
+  servicioId: any,
+  token: string
+) => {
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
 
-  await axios.get(
-      `https://reparame-api.onrender.com/servicios${servicioId ? "/" + servicioId : ''}`, {
-        headers: { Authorization: `Bearer ${token}` }
+  await axios
+    .get(
+      `https://reparame-api.onrender.com/servicios${
+        servicioId ? "/" + servicioId : ""
+      }`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
       }
     )
     .then(function (response) {
@@ -17,12 +25,4 @@ export const getServicios = async (setPrestadores: Function, servicioId: any, to
       console.log("error");
       console.log(err);
     });
-        .then(function (response) {
-            setPrestadores(response.data)
-        })
-        .catch(function (err) {
-            console.log('error')
-            console.log(err);
-        })
-
 };
