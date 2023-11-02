ecd6caad1b50464da7efe29bf33a3c58611b9e0b
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-11-02 03:37:00 -0300

    fix post ticket

--- frontend/reparame/src/utils/requestTicket/postTicket.ts
+++ frontend/reparame/src/utils/requestTicket/postTicket.ts
@@ -1,14 +1,12 @@
 import axios from "axios"
 
-export const postTicket = async (id: any, formData: any,) => {
-    const tok = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2FAZ21haWwuY29tIiwiaWF0IjoxNjk4NzY0OTMxLCJleHAiOjE2OTg3NjYzNzF9.5iVpTsMkVp0ZbAJgfEXXUImqzeX7p27IKMypKk6dPVY'
-
+export const postTicket = async (id: any, formData: any, token: any) => {
     await axios.post(
         `https://reparame-api.onrender.com/tickets/${id}`, {
         formData
     }, {
         headers: {
-            Authorization: `Bearer ${tok}`
+            Authorization: `Bearer ${token}`
 
         }
     }
